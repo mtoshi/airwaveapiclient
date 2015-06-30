4ba11dc5ed07e6d4d6574b3165db7c0d22d43e90
mtoshi <mtoshi.g@gmail.com>
2015-06-30 20:18:32 +0900

    Change package import setting.

--- airwaveapiclient/__init__.py
+++ airwaveapiclient/__init__.py
@@ -7,5 +7,9 @@ import sys
 # pylint: disable=unused-import
 if sys.version_info.major == 2:
     from airwaveapiclient import AirWaveAPIClient
+    from airwaveapiclient import APList
+    from airwaveapiclient import APNode
 else:
     from airwaveapiclient.airwaveapiclient import AirWaveAPIClient
+    from airwaveapiclient.airwaveapiclient import APList
+    from airwaveapiclient.airwaveapiclient import APNode
