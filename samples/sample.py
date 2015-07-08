9427bb9fdb0c826a49052c2fc692b5465c6aeb6c
mtoshi <mtoshi.g@gmail.com>
2015-07-09 00:44:54 +0900

    Add sample code.

--- samples/sample.py
+++ samples/sample.py
@@ -3,8 +3,62 @@
 """AirWaveAPIClient sample."""
 
 
+from airwaveapiclient import AirWaveAPIClient
+from airwaveapiclient import APList
+from airwaveapiclient import APDetail
+from pprint import pprint
+
+
 def main():
     """Sample main."""
+    #################################################
+    # Settings ######################################
+    #################################################
+
+    username = 'admin'
+    password = '*****'
+    address = '192.168.1.1'
+
+    #################################################
+    # Login #########################################
+    #################################################
+
+    airwave = AirWaveAPIClient(username=username,
+                               password=password,
+                               address=address)
+    airwave.login()
+
+    #################################################
+    # APList ########################################
+    #################################################
+
+    res = airwave.ap_list()
+    if res.status_code == 200:
+        xml = res.text
+        ap_list = APList(xml)
+        for ap_node in ap_list:
+            pprint('-'*80)
+            pprint(ap_node)
+            pprint('-'*80)
+
+    #################################################
+    # APDetail ######################################
+    #################################################
+
+    for ap_node in ap_list:
+        res = airwave.ap_detail(ap_node['@id'])
+        if res.status_code == 200:
+            xml = res.text
+            ap_detail = APDetail(xml)
+            pprint('-'*80)
+            pprint(ap_detail)
+            pprint('-'*80)
+
+    #################################################
+    # Logout ########################################
+    #################################################
+
+    airwave.logout()
 
 if __name__ == "__main__":
 
