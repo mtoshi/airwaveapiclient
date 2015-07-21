35439573298045dad02d5cc6e98574fa0b5e8032
mtoshi <mtoshi.g@gmail.com>
2015-07-21 20:51:11 +0900

    Add dot11_counters graph into APGraph.

--- airwaveapiclient/ap_graph.py
+++ airwaveapiclient/ap_graph.py
@@ -175,6 +175,52 @@ class APGraph(OrderedDict):
         """
         return self.__ap_graph(u'ap_bandwidth', u'aN', start, end)
 
+    def dot11_counters_802dot11bgn(self, start=None, end=None):
+        """RRD graph URL for access point dot11 counters of radio type IEEE802.11BGN.
+
+        Args:
+
+            :start (int, optional): Graph start time(seconds ago).
+                Default is None.
+            :end (int, optional): Graph end time(seconds ago).
+                Default is None.
+
+        Returns:
+
+            :str: Graph URL string.
+
+        Usage: ::
+
+            >>> ap_graph.dot11_counters_802dot11bgn(start=-3600)
+            'https://x.x.x.x/nf/rrd_graph?
+                end=-0s&id=1&radio_index=1&start=-3600s&type=dot11_counters'
+
+        """
+        return self.__ap_graph(u'dot11_counters', u'bgn', start, end)
+
+    def dot11_counters_802dot11an(self, start=None, end=None):
+        """RRD graph URL for access point dot11 counters of radio type IEEE802.11AN.
+
+        Args:
+
+            :start (int, optional): Graph start time(seconds ago).
+                Default is None.
+            :end (int, optional): Graph end time(seconds ago).
+                Default is None.
+
+        Returns:
+
+            :str: Graph URL string.
+
+        Usage: ::
+
+            >>> ap_graph.dot11_counters_802dot11an(start=-3600)
+            'https://x.x.x.x/nf/rrd_graph?
+                end=-0s&id=1&radio_index=2&start=-3600s&type=dot11_counters'
+
+        """
+        return self.__ap_graph(u'dot11_counters', u'aN', start, end)
+
     def __radio_graph(self, graph_type, radio_type, start, end):
         """RRD access point graph base method.
 
