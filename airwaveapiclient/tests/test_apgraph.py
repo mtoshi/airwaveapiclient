649ec090cda3385571da38de837f59ce6688c289
mtoshi <mtoshi.g@gmail.com>
2015-07-24 20:11:14 +0900

    Add test code of APGraph radio errors.

--- airwaveapiclient/tests/test_apgraph.py
+++ airwaveapiclient/tests/test_apgraph.py
@@ -198,3 +198,29 @@ class APGraphUnitTests(unittest.TestCase):
                       'start=-7200s&'
                       'type=radio_power')
         self.assertEqual(graph_url, _graph_url)
+
+    def test_radio_errors_802dot11bgn(self):
+        """Test for radio errors 802dot11bgn."""
+        ap_graph = APGraph(self.url, self.objs[0])
+        graph_url = ap_graph.radio_errors_802dot11bgn()
+        _graph_url = ('https://192.168.1.1/nf/rrd_graph?'
+                      'ap_uid=00%3A00%3A10%3A00%3A00%3A01&'
+                      'end=0s&'
+                      'radio_index=1&'
+                      'radio_interface=2&'
+                      'start=-7200s&'
+                      'type=radio_errors')
+        self.assertEqual(graph_url, _graph_url)
+
+    def test_radio_errors_802dot11an(self):
+        """Test for radio errors 802dot11an."""
+        ap_graph = APGraph(self.url, self.objs[0])
+        graph_url = ap_graph.radio_errors_802dot11an()
+        _graph_url = ('https://192.168.1.1/nf/rrd_graph?'
+                      'ap_uid=00%3A00%3A10%3A00%3A00%3A01&'
+                      'end=0s&'
+                      'radio_index=2&'
+                      'radio_interface=1&'
+                      'start=-7200s&'
+                      'type=radio_errors')
+        self.assertEqual(graph_url, _graph_url)
