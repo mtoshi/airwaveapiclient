ad5dba78b53d7978bfdde36f8abb5aca5005d1cb
mtoshi <mtoshi.g@gmail.com>
2015-08-17 18:33:50 +0900

    Add dot11_counters_802dot11ac graph

--- airwaveapiclient/ap_graph.py
+++ airwaveapiclient/ap_graph.py
@@ -293,6 +293,29 @@ class APGraph(OrderedDict):
         """
         return self.__ap_graph(u'dot11_counters', u'aN', start, end)
 
+    def dot11_counters_802dot11ac(self, start=None, end=None):
+        """RRD graph URL for access point dot11 counters of radio type IEEE802.11AC.
+
+        Args:
+
+            :start (int, optional): Graph start time(seconds ago).
+                Default is None.
+            :end (int, optional): Graph end time(seconds ago).
+                Default is None.
+
+        Returns:
+
+            :str: Graph URL string.
+
+        Usage: ::
+
+            >>> ap_graph.dot11_counters_802dot11ac(start=-3600)
+            'https://x.x.x.x/nf/rrd_graph?
+                end=-0s&id=3&radio_index=2&start=-3600s&type=dot11_counters'
+
+        """
+        return self.__ap_graph(u'dot11_counters', u'ac', start, end)
+
     def __radio_graph(self, graph_type, radio_type, start, end):
         """RRD access point graph base method.
 
--- airwaveapiclient/tests/test_apgraph.py
+++ airwaveapiclient/tests/test_apgraph.py
@@ -148,6 +148,18 @@ class APGraphUnitTests(unittest.TestCase):
                       'type=dot11_counters')
         self.assertEqual(graph_url, _graph_url)
 
+    def test_dot11_counters_802dot11ac(self):
+        """Test for dot11 counters 802dot11ac."""
+        ap_graph = APGraph(self.url, self.objs[2])
+        graph_url = ap_graph.dot11_counters_802dot11ac()
+        _graph_url = ('https://192.168.1.1/nf/rrd_graph?'
+                      'end=0s&'
+                      'id=3&'
+                      'radio_index=2&'
+                      'start=-7200s&'
+                      'type=dot11_counters')
+        self.assertEqual(graph_url, _graph_url)
+
     def test_radio_channel_802dot11bgn(self):
         """Test for channel 802dot11bgn."""
         ap_graph = APGraph(self.url, self.objs[0])
